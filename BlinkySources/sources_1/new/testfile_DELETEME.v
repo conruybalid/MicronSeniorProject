52e34ffc28afdecb7f56a3603322c48fb1ba562b
Connor Ruybalid <CRuybalid@my.nnu.edu>
2024-11-18 16:34:30 -0700

    quick file creation test with GUI

---
+++ BlinkySources/sources_1/new/testfile_DELETEME.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/18/2024 04:32:56 PM
+// Design Name: 
+// Module Name: testfile_DELETEME
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+//Just testing how this stuff works
+
+
+module testfile_DELETEME(
+    input in,
+    output out
+    );
+endmodule
